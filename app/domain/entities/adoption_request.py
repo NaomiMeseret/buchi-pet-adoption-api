fadd4790ade0b7d0d42e5ffac921ed21567b11b7
NaomiMeseret <naomimu2121@gmail.com>
2026-04-16 11:54:36 +0300

    feat(domain): add status to adoption requests

--- app/domain/entities/adoption_request.py
+++ app/domain/entities/adoption_request.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from datetime import datetime
 
+from app.domain.enums.adoption_request_status import AdoptionRequestStatus
+
 
 @dataclass
 class AdoptionRequest:
@@ -8,3 +10,4 @@ class AdoptionRequest:
     customer_id: str
     pet_id: str
     created_at: datetime
+    status: AdoptionRequestStatus = AdoptionRequestStatus.PENDING
