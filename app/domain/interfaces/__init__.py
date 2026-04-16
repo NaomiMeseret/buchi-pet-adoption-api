2d564728626c02360281a0a952ae607285b6cfdf
NaomiMeseret <naomimu2121@gmail.com>
2026-04-16 13:01:04 +0300

    chore(domain): export updated domain interfaces

--- app/domain/interfaces/__init__.py
+++ app/domain/interfaces/__init__.py
@@ -1,6 +1,7 @@
 from app.domain.interfaces.adoption_repository import AdoptionRepository
 from app.domain.interfaces.customer_repository import CustomerRepository
 from app.domain.interfaces.external_dog_provider import ExternalDogProvider
+from app.domain.interfaces.file_storage import FileStorage
 from app.domain.interfaces.pet_repository import PetRepository
 
 __all__ = [
@@ -8,4 +9,5 @@ __all__ = [
     "CustomerRepository",
     "AdoptionRepository",
     "ExternalDogProvider",
+    "FileStorage",
 ]
